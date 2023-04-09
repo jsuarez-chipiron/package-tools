ff6a2e4b170f84f2752cb887508c9d173cf7be12
Javier Suarez <jsuarez@salesforce.com>
2023-04-09 21:56:32 +0200

    enhance inner types

--- main.cpp
+++ main.cpp
@@ -7,12 +7,6 @@
 #include <unordered_map>
 #include <vector>
 
-struct type_element
-{
-    std::string name;
-    std::set<std::string> members;
-};
-
 std::string reader(const std::string& filename)
 {
     std::ifstream ifs (filename, std::ifstream::in);
@@ -34,7 +28,7 @@ std::string reader(const std::string& filename)
     return result;
 }
 
-void process_type(std::string_view content, std::unordered_map<std::string, type_element>& map, std::set<std::string>& index)
+void process_type(std::string_view content, std::unordered_map<std::string, std::set<std::string>>& map, std::set<std::string>& index)
 {
     auto start_idx = content.find("<name>");
     auto end_idx = content.find("</name>");
@@ -42,31 +36,20 @@ void process_type(std::string_view content, std::unordered_map<std::string, type
     auto name = content.substr(start_idx+6, end_idx-start_idx-6);
     index.emplace(name);
 
-    std::set<std::string> members;
-
     start_idx = content.find("<members>");
     while ( start_idx != std::string_view::npos )
     {
         content = content.substr(start_idx+9);
         end_idx = content.find("</members>");
         auto member = content.substr(0, end_idx);
-        members.emplace(member);
+        map[std::string(name)].emplace(member);
         content = content.substr(end_idx+10);
         start_idx = content.find("<members>");
     }
-    type_element te;
-    te.name = std::string(name);
-    te.members = std::move(members);
-
-    if ( map.contains(te.name) )
-    {
-        te.members.insert(map.at(te.name).members.begin(), map.at(te.name).members.end());
-    }
-    map[te.name] = te;
 }
 
 void parse(std::string content, 
-        std::unordered_map<std::string, type_element>& map, std::set<std::string>& index)
+        std::unordered_map<std::string, std::set<std::string>>& map, std::set<std::string>& index)
 {
     auto start_idx = content.find("<types>");
 
@@ -79,19 +62,18 @@ void parse(std::string content,
     }
 }
 
-void print(const std::unordered_map<std::string, type_element>& map, const std::set<std::string>& index)
+void print(const std::unordered_map<std::string, std::set<std::string>>& map, const std::set<std::string>& index)
 {
     std::cout << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n";
     std::cout << "<Package xmlns=\"http://soap.sforce.com/2006/04/metadata\">\n";
-    for (const auto& i: index)
+    for (const auto& name: index)
     {
         std::cout << "    <types>\n";
-        type_element te = map.at(i);
-        for (const auto& j: te.members)
+        for (const auto& member: map.at(name))
         {
-            std::cout << "        <members>" << j << "</members>\n";
+            std::cout << "        <members>" << member << "</members>\n";
         }
-        std::cout << "        <name>" << te.name << "</name>\n";
+        std::cout << "        <name>" << name << "</name>\n";
         std::cout << "    </types>\n";
     }
 
@@ -104,7 +86,7 @@ int main(int /*argc*/, char ** /*argv*/)
     auto file_content_1 = reader("/home/javier/Tech/c++/flattern/package.xml");
     auto file_content_2 = reader("/home/javier/Tech/c++/flattern/mini.xml");
 
-    std::unordered_map<std::string, type_element> items_map;
+    std::unordered_map<std::string, std::set<std::string>> items_map;
     std::set<std::string> items_idx;
 
     parse(file_content_1, items_map, items_idx);
